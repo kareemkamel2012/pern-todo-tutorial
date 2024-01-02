95e24e41ffd926029bb57c430cf1b8d625d66a67
Kareem <kareemkamel2012@gmail.com>
2024-01-01 21:27:47 -0600

    complete frontend

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 //components
 
 import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
 function App() {
   return(
     <Fragment>
       <div className='container'>
+        <h1 className='text-center mt-5'>Todo List</h1>
         <InputTodo />
+        <ListTodos />
       </div>
     </Fragment>
   );
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -8,13 +8,13 @@ const InputTodo = () => {
         e.preventDefault();
         try {
             const body = { description };
-            const response = fetch('http://localhost:5000/todos', { 
+            const response = await fetch('http://localhost:4200/todos', { 
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(body)
             });
 
-            console.log(response);
+            window.location = '/'
         } catch (err) {
             console.error(err.message);
         }
@@ -22,8 +22,7 @@ const InputTodo = () => {
 
     return (
         <Fragment>  
-            <h1 className='text-center mt-5'>Input Todo</h1>
-            <form className='d-flex mt-5'>
+            <form className='d-flex mt-5' onSubmit={onSubmitForm}>
                 <input type='text' className='form-control' value={description} 
                 onChange={e =>setDescription(e.target.value)} />
                 <button className='btn btn-success'>Add</button>
